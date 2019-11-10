f0ee20ac3cc99f05cc360cc503d4d3abf1d652c4
juliano <julianoferrasso@gmail.com>
2019-11-10 14:54:23 -0300

    atualizado arquivos

--- src/server.js
+++ src/server.js
@@ -4,16 +4,11 @@ const routes = require ('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://juliano:juliano@nomadwe-kxpnw.mongodb.net/nomadwe?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://userdbmongo:admin@dbmongo-kxpnw.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
-// GET, POST, PUT, DELETE
-
-// req.query = Acessar query paramas (para filtros "get do php")
-// req.params = Acessar route params (para edicao, delete)
-// req.body = Acessar corpo da requisisção (para criacao, edicao)
 
 app.use(express.json());
 app.use(routes);
