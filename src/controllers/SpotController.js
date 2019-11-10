a9360d45f484e90f9c878c40144d5bffdfa26e45
juliano <julianoferrasso@gmail.com>
2019-11-10 18:53:48 -0300

    atualizado arquivos

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -12,9 +12,40 @@
 // req.params = Acessar route params (para edicao, delete)
 // req.body = Acessar corpo da requisisção (para criacao, edicao)
 
+const Spot = require('../models/Spot');
+const User = require('../models/User');
+
 
 module.exports = {
+    // lista todos os Spots
+    async index(req, res) {
+        const { tech } = req.query;
+
+        const spots = await Spot.find({ techs: tech})
+
+        return res.json(spots);
+    },
+
+    //cria Spot
     async store(req, res) {
-        return res.json({ ok: true})
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User does not exists' });
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map( tech => tech.trim()),
+            price
+        })
+
+        return res.json(spot)
     }
 };
